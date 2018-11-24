e1f647098f2d05f06958aa6ca7441fcd52c6019d
sanitea <33693436+sanitea@users.noreply.github.com>
2018-11-24 20:03:32 +0000

    It works and the animation is smooth!

--- agentframework.py
+++ agentframework.py
@@ -5,15 +5,14 @@ import matplotlib.pyplot
 #Agent class
 class Agent():
         
-        def __init__ (self, environment):
-            self.x = random.randint(0,250)
+        def __init__ (self, environment, agents):
+            self.x = random.randint(0,220)
             self.y = random.randint(0,250)
             
             self.environment = environment
             self.store = 0
             
-            self.agents_list = []
-
+            self.agents = agents
             self.num_of_iterations = 100
             self.num_of_agents = 10 
             
@@ -22,35 +21,36 @@ class Agent():
             
             self.agents_row_a = [self.x, self.y]
             self.agents_row_b = [self.x, self.y]
+            
+            self.dead = False 
         
-        def distance_between(self, agents):
-                return (((self.x - agents.x)**2) + ((self.y - agents.y)**2))**0.5 
+        def distance_between(self, agent):
+                return (((self.x - agent.x)**2) + ((self.y - agent.y)**2))**0.5 
             
         #def update(num_of_iterations):
-        
-        def pop(self, agents):
-            agents_list.append(agents)
+            
             
             
         def move(self):
     #Agents Moving. Modulus operator = boundaries using torus method. 
-            for j in range (self.num_of_iterations):
-                for i in range (self.num_of_agents):
-                    if random.random() < 0.5:
-                        agents[i].y  = (agents[i].y + 1) % 99 
-                    else:
-                        agents[i].y  = (agents[i].y - 1) % 99
+           # for j in range (self.num_of_iterations):
+           for i in range (self.num_of_agents):
+               if random.random() < 0.5:
+                   self.y = (self.y + 1) % 300
+               else:
+                   self.y = (self.y - 1) % 300
+                        
+               if random.random() < 0.5:
+                   self.x = (self.x + 1) % 300
+               else:
+                   self.x = (self.x - 1) % 300 
                     
-                    if random.random() < 0.5:
-                        agents[i].x  = (agents[i].x + 1) % 99 
-                    else:
-                        agents[i].x  = (agents[i].x - 1) % 99 
                         
             
         def eat(self): 
             if self.environment[self.y][self.x] > 10:
-                self.environment[self.y][self.x] - 10
-                self.store + 1
+                self.environment[self.y][self.x] -= 100
+                self.store += 1
               #  print(self.store)
          #   else:
               #  print('cats')
@@ -63,6 +63,8 @@ class Agent():
                     ave = sum /2
                     self.store = ave
                     agent.store = ave
+                if agent.store < 2:
+                    del agent
                   #  print(self.store)
                    # print("sharing " + str(dist) + " " + str(ave))
             
@@ -73,4 +75,3 @@ class Agent():
 #def update(num_of_iterations):
     
 #animation = matplotlib.animation.FuncAnimation(fig, update, interval=1, repeat=False, frames=num_of_iterations)
-        
--- model.py
+++ model.py
@@ -8,6 +8,8 @@ import agentframework
 import csv 
 import matplotlib.animation 
 
+
+
 def distance_between(agents_row_a, agents_row_b):
     return (((agents_row_a.x - agents_row_b.x)**2) + 
     ((agents_row_a.y - agents_row_b.y)**2))**0.5
@@ -21,7 +23,7 @@ wolves = []
 g = 'Sheep'
 
 fig = matplotlib.pyplot.figure(figsize=(7, 7))
-ax = fig.add_axes([0, 0, 1, 1])
+#ax = fig.add_axes([0, 0, 1, 1])
 
 
 #Environment. Empty row, append rowlist to environment, append rowlist. 
@@ -36,41 +38,34 @@ f.close()
 
 # Make the agents.
 for i in range(num_of_agents):
-    agents.append(agentframework.Agent(environment))
-    agents.pop(agents)
+    agents.append(agentframework.Agent(environment, agents))
+#    wolves.append(wolfClass.Wolf(environment))
     agents[i].share_with_neighbours(neighbourhood, agents)
 
 # Move the agents.
 def update (frame_number):
+
     for i in range (frame_number):
+        fig.clear()
         for j in range(num_of_agents):
-            
-            fig.clear()
-            
+
             random.shuffle(agents)
             agents[j].move()
             agents[j].eat()
             agents[j].share_with_neighbours(neighbourhood, agents)
             matplotlib.pyplot.imshow(environment)
+        for k in range(0,len(agents)):
 
-        for j in range(0,len(agents)):
-            matplotlib.pyplot.scatter(agents[j],agents[j], marker="D",  color='white')
-            matplotlib.show()
-
-animation = matplotlib.animation.FuncAnimation(fig, update, interval=1, frames=frame_number, repeat=False)
-
-#root = tkinter.Tk()
-#root.wm_title("Model")
-#canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(fig, master=root)
-#canvas._tkcanvas.pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1) 
-
-#menu_bar = tkinter.Menu(root)
-#root.config(menu=menu_bar)
-#model_menu = tkinter.Menu(menu_bar)
-#menu_bar.add_cascade(label="Model", menu=model_menu)
-#model_menu.add_command(label="Run model", command=run) 
+                        
+            matplotlib.pyplot.scatter(agents[k].x,agents[k].y, marker="D",  color='white')
+            matplotlib.pyplot.show()
 
 
-#tkinter.mainloop() 
+    
+
+animation = matplotlib.animation.FuncAnimation(fig, update, frames=frame_number, repeat=False)
+#canvas.show() 
+
+
 ##"""Check if wolf appears -> spawn wolf -> 
 #Wolf checks distance between him and sheep, heads towards closest sheep, if within 10 wolf eats sheep 
